01a3c79e9681b03e1213c4776a9446c37b374cf3
MarioGarza <montelong0@outlook.com>
2016-10-02 18:41:08 -0500

    Commit

--- app/src/main/java/com/example/mario/dispositivosmoviles/antros.java
+++ app/src/main/java/com/example/mario/dispositivosmoviles/antros.java
@@ -1,11 +1,13 @@
 package com.example.mario.dispositivosmoviles;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageButton;
 
 public class antros extends AppCompatActivity {
 
@@ -16,6 +18,15 @@ public class antros extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        ImageButton dclub = (ImageButton) findViewById(R.id.dclub);
+        dclub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(antros.this, infoDclub.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
