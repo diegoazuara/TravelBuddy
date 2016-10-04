51bed97293d4529f670d725b18f1750f43614ced
diegoazuara <d_azuara@live.com.mx>
2016-10-04 11:21:21 -0500

    Google Maps
    - Se ve la localización de D Club al presionar el botón.

--- app/src/main/java/com/example/mario/dispositivosmoviles/infoDclub.java
+++ app/src/main/java/com/example/mario/dispositivosmoviles/infoDclub.java
@@ -1,13 +1,21 @@
 package com.example.mario.dispositivosmoviles;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class infoDclub extends AppCompatActivity {
+import java.net.URL;
+
+public class infoDclub extends AppCompatActivity{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +23,22 @@ public class infoDclub extends AppCompatActivity {
         setContentView(R.layout.activity_info_dclub);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Button botonDclub = (Button)findViewById(R.id.button);
+
+        botonDclub.setOnClickListener(new View.OnClickListener() {
+                                          @Override
+                                          public void onClick(View view) {
+                                              Intent intent = new Intent();
+                                              intent.setAction(Intent.ACTION_VIEW);
+                                              intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                                              intent.setData(Uri.parse("https://www.google.com.mx/maps/place/D+Club+Monterrey/@25.6597522,-100.3699162,17z/data=!3m1!4b1!4m5!3m4!1s0x8662bdf2c5d1c71b:0x8f34550e8270d161!8m2!3d25.6597522!4d-100.3677275"));
+                                              startActivity(intent);
+                                          }
+                                      }
+
+        );
+
 
 
     }
