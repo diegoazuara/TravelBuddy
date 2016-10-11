8a58f5ebbd6b859b8845daaa6974d3f5e8844eea
23marioley <marioleyt@hotmail.com>
2016-10-10 21:08:23 -0500

    Info plaza cumbres

---
+++ app/src/main/java/com/example/mario/dispositivosmoviles/infoPlazaCumbres.java
@@ -0,0 +1,14 @@
+package com.example.mario.dispositivosmoviles;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class infoPlazaCumbres extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_plaza_cumbres);
+    }
+
+}
