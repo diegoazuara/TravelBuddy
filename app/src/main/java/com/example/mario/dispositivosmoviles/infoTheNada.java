a74859346d61911ef9466a757b7e3b37ff29155b
23marioley <marioleyt@hotmail.com>
2016-10-21 20:30:44 -0500

    bla

--- app/src/main/java/com/example/mario/dispositivosmoviles/infoTheNada.java
+++ app/src/main/java/com/example/mario/dispositivosmoviles/infoTheNada.java
@@ -1,31 +1,23 @@
 package com.example.mario.dispositivosmoviles;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.Button;
 
-public class cafeterias extends AppCompatActivity {
+public class infoTheNada extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cafeterias);
+        setContentView(R.layout.activity_info_the_nada);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        ImageButton thenada = (ImageButton) findViewById(R.id.thenada);
-        thenada.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(cafeterias.this, infoTheNada.class);
-                startActivity(intent);
-            }
-        });
     }
 
 }
