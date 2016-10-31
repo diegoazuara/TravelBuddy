d7ee29c49a0a762409056b32ae91e39574abe41d
23marioley <marioleyt@hotmail.com>
2016-10-31 01:01:02 -0600

    colores comida

--- app/src/main/java/com/example/mario/dispositivosmoviles/infoPuntaCielo.java
+++ app/src/main/java/com/example/mario/dispositivosmoviles/infoPuntaCielo.java
@@ -30,7 +30,6 @@ public class infoPuntaCielo extends AppCompatActivity {
                                                 startActivity(intent);
                                             }
                                         }
-
         );
     }
 
